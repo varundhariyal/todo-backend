45bc672aa469a6276d460a0cd087819f867dd956
varundhariyal <varundhariyal@gmail.com>
2019-06-03 01:42:19 +0530

    apidoc.json

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -1,13 +1,15 @@
 define({
-  "name": "live-to-do-usermanagement",
+  "name": "TODO Application Backend",
   "version": "1.0.0",
-  "description": "RESTful API for user-management for Live-To-Do",
+  "description": "API Documentation for the TODO app",
+  "title": "TODO APIs",
+  "url": "http://api.livetodo.xyz",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-06-02T19:44:56.290Z",
+    "time": "2019-06-02T20:11:38.647Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
