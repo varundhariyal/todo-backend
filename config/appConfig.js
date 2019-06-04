df43cfc4b0b3cf0cbfaec801828852a44d6b4bda
varundhariyal <varundhariyal@gmail.com>
2019-06-05 00:49:44 +0530

    prod

--- config/appConfig.js
+++ config/appConfig.js
@@ -6,7 +6,7 @@ appConfig.env = "dev";
 appConfig.db = {
     uri: 'mongodb://127.0.0.1:27017/userManageDB1'
   }
-appConfig.apiVersion = '/api/v1';
+appConfig.apiVersion = '/v1';
 
 
 module.exports = {
